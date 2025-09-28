40a853abcdbfa0f4e00138f8e5b8259d0016bdf4
pleasantvik <adedayo.ayoola5683@gmail.com>
2025-09-28 10:41:24 +0100

    multiple pointers

---
+++ 002-Multiple-Pointers/002-countUniqueValues/countUniqueValue.spec.js
@@ -0,0 +1,16 @@
+import { countUniqueValue } from "./countUniqueValues";
+import { describe, expect, it } from "vitest";
+describe("Test", () => {
+  it("Test 1", () => {
+    const result = countUniqueValue([]);
+    expect(result).toBe(0);
+  });
+  it("Test 2", () => {
+    const result = countUniqueValue([1, 1, 2]);
+    expect(result).toBe(2);
+  });
+  it("Test 3", () => {
+    const result = countUniqueValue([1, 2, 2, 3, 4, 4, 5, 6]);
+    expect(result).toBe(6);
+  });
+});
